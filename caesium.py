0984cccfe44970f9ff6b5fda1b42437849581e30
Andrew Lobanov <spline@rooker.ru>
2015-07-08 10:18:37 +0500

    Все файлы объеденены в один и добавлен фетчинг сообщений.

--- caesium.py
+++ caesium.py
@@ -1,9 +1,105 @@
 #!/usr/bin/env python3
 
-import curses
+import curses, os, urllib.request, base64, codecs
 from datetime import datetime
-import lib.base as base
-import lib.ii as ii
+
+node = ""
+auth = ""
+echoes = []
+
+def check_directories():
+    if not os.path.exists("echo"):
+        os.mkdir("echo")
+    if not os.path.exists("msg"):
+        os.mkdir("msg")
+    if not os.path.exists("out"):
+        os.mkdir("out")
+
+#
+# Взаимодействие с нодой
+#
+
+def separate(l, step=20):
+    for x in range(0, len(l), step):
+        yield l[x:x+step]
+        
+def load_config():
+    global node, auth, echoes
+    f = open("caesium.cfg", "r")
+    config = f.read().split("\n")
+    f.close()
+    for line in config:
+        param = line.split(" ")
+        if param[0] == "node":
+            node = param[1]
+        elif param[0] == "auth":
+            auth = param[1]
+        elif param[0] == "echo":
+            if len(param) > 2:
+                echoes.append([param[1], " ".join(param[2:])])
+            else:
+                echoes.append([param[1], ""])
+
+def get_msg_list(echo):
+    msg_list = []
+    r = urllib.request.Request(node + "u/e/" + echo[0])
+    with urllib.request.urlopen(r) as f:
+        lines = f.read().decode("utf-8").split("\n")
+        for line in lines:
+            if line != echo:
+                msg_list.append(line)
+    return msg_list
+
+def get_local_msg_list(echo):
+    if not os.path.exists("echo/" + echo[0]):
+        return []
+    else:
+        local_msg_list = codecs.open("echo/" + echo[0], "r", "utf-8").read().split("\n")
+        return local_msg_list
+
+def get_bundle(msgids):
+    bundle = []
+    r = urllib.request.Request(node + "u/m/" + msgids)
+    with urllib.request.urlopen(r) as f:
+        bundle = f.read().decode("utf-8").split("\n")
+    return bundle
+
+def debundle(echo, bundle):
+    for msg in bundle:
+        if msg:
+            m = msg.split(":")
+            msgid = m[0]
+            if len(msgid) == 20 and m[1]:
+                codecs.open("msg/" + msgid, "w", "utf-8").write(base64.b64decode(m[1]).decode("utf-8"))
+                codecs.open("echo/" + echo[0], "a", "utf-8").write(msgid + "\n")
+
+def fetch_mail():
+    stdscr.clear()
+    stdscr.attron(curses.color_pair(1))
+    stdscr.attron(curses.A_BOLD)
+    stdscr.border()
+    draw_title(0, 1, "Получение почты")
+    line = 0
+    for echo in echoes:
+        line = line + 1
+        remote_msg_list = get_msg_list(echo)
+        if len(remote_msg_list) > 1:
+            local_msg_list = get_local_msg_list(echo)
+            msg_list = [x for x in remote_msg_list if x not in local_msg_list]
+            list_len = len (msg_list)
+            n = 0
+            for get_list in separate(msg_list):
+                debundle(echo, get_bundle("/".join(get_list)))
+                n = n + len(get_list)
+                stdscr.addstr(line, 1, "Получение сообщений из " + echo[0] + ": " + str(n) + "/" + str(list_len), curses.color_pair(4))
+                stdscr.refresh()
+        else:
+            codecs.open("echo/" + echo, "a", "utf-8").close()
+    stdscr.clear()
+
+#
+# Пользовательский интерфейс
+#
 
 echo_cursor = 0
 
@@ -26,7 +122,7 @@ def draw_echo_selector(start):
     stdscr.border()
     draw_title(0, 1, "Выбор эхоконференции")
     y = 0
-    for echo in ii.echoes:
+    for echo in echoes:
         if y - start < height - 2:
             if y == echo_cursor:
                 if y >= start:
@@ -62,13 +158,15 @@ def echo_selector():
             echo_cursor = echo_cursor - 1
             if echo_cursor - start < 0 and start > 0:
                 start = start - 1
-        elif key == curses.KEY_DOWN and echo_cursor < len(ii.echoes) - 1:
+        elif key == curses.KEY_DOWN and echo_cursor < len(echoes) - 1:
             echo_cursor = echo_cursor + 1
-            if echo_cursor - start > height - 3 and start < len(ii.echoes) - height + 2:
+            if echo_cursor - start > height - 3 and start < len(echoes) - height + 2:
                 start = start + 1
+        elif key == ord("g"):
+            fetch_mail()
 
-base.check_directories()
-ii.load_config()
+check_directories()
+load_config()
 stdscr = curses.initscr()
 curses.start_color()
 curses.noecho()
--- lib/base.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def check_directories():
-    if not os.path.exists("echo"):
-        os.mkdir("echo")
-    if not os.path.exists("msg"):
-        os.mkdir("msg")
-    if not os.path.exists("out"):
-        os.mkdir("out")
